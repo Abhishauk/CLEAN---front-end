f8128dec55156a9985cb0b0da9a790a1c41989ec
Abhishauk <ukabhisha@gmail.com>
2024-04-17 19:58:08 +0530

    client setup commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+
+import './App.css';
+import {BrowserRouter as Router , Route , Routes} from 'react-router-dom';
+import Home from './components/Home';
+
+function App() {
+ 
+  return (
+    <Router>
+    <div className="App">
+      <Routes>
+        <Route path='/' element = {< Home/>}/>
+      </Routes>
+     
+    
+    </div>
+    </Router>
+  );
+  
+}
+
+export default App;
